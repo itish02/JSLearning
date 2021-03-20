af0fbcb3c6c0062c60ea18518382ead8a9889917
iti <itish.02@gmail.com>
2021-03-20 19:05:45 +0530

    Clean code commit

--- JavaScript/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/clean.js
+++ JavaScript/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,45 +1,74 @@
-var sc = [
-  { product: 'bread', quantity: 6 },
-  { product: 'pizza', quantity: 2 },
-  { product: 'milk', quantity: 4 },
-  { product: 'water', quantity: 10 },
-];
+'strict mode'
+const budget = Object.freeze([
+    { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+    { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+    { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+    { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+    { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+    { value: -20, description: 'Candy 🍭', user: 'matilda' },
+    { value: -125, description: 'Toys 🚂', user: 'matilda' },
+    { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+]);
 
-var allow = {
-  lisbon: 5,
-  others: 7,
+const checkLimit = (limits, user) => limits ? .[user] ? ? 0;
+
+const spendingLimits = Object.freeze({
+    jonas: 1000,
+    matilda: 100,
+});
+
+spendingLimits.jay = 500;
+console.log(spendingLimits);
+
+const addExpense = function(
+    state,
+    limits,
+    value,
+    description,
+    user = 'jonas'
+) {
+    // if (!user) user = 'jonas';
+    const cleanUser = user.toLowerCase();
+    // const limit = checkLimit(user);
+    // if (limits[user]) {
+    //   lim = limits[user];
+    // } else {
+    //   lim = 0;
+    // }
+
+    return (value <= checkLimit(limits, cleanUser)) ? [...state, { value: -value, description, user: cleanUser }] : state;
+    // budget.push({ value: -value, description, user: cleanUser });
+
+};
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'pizza 🍕');
+const newBudget2 = addExpense(newBudget1, spendingLimits, 10, 'Going to movies 🍿', 'Matilda');
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+// console.log(newBudget1);
+// console.log(newBudget2);
+
+// const checkExpenses = function(state, limits) {
+//   return state.map(entry => {
+//     return entry.value < -checkLimit(limits, entry.user) ? {...entry, flag: 'limit'} : entry;
+//   }); 
+// };
+
+const checkExpenses = (state, limits) => {
+    state.map(entry => entry.value < -checkLimit(limits, entry.user) ? {...entry, flag: 'limit' } : entry);
 };
 
-var description = '';
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
 
-var check = function (city) {
-  if (sc.length > 0) {
-    var allowed;
-    if (city == 'lisbon') {
-      allowed = allow.lisbon;
-    } else {
-      allowed = allow.others;
-    }
+// console.log(budget);
 
-    for (item of sc) {
-      if (item.quantity > allowed) item.quantity = allowed;
-    }
-  }
+const bigExpenses = function(spendingLimit) {
+    let output = '';
+    for (let entry of budget)
+        output +=
+        entry.value <= -spendingLimit ? `${entry.description.slice(-2)} / ` : '' // Emojis are 2 chars
+
+    output = output.slice(0, -2); // Remove last '/ '
+    console.log(output);
 };
-check('lisbon');
-console.log(sc);
 
-var createDescription = function () {
-  var first = sc[0];
-  var p = first.product;
-  var q = first.quantity;
-
-  if (sc.length > 1) {
-    description = 'Order with ' + q + ' ' + p + ', etc...';
-  } else {
-    description = 'Order with ' + q + ' ' + p + '.';
-  }
-};
-createDescription();
-
-console.log(description);
+bigExpenses(100);
